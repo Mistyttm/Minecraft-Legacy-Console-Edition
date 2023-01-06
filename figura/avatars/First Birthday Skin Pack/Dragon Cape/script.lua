3a3bcde61661e9f87d48145b90075401eb025470
Emmey <51770769+Mistyttm@users.noreply.github.com>
2023-01-06 21:54:59 +1000

    Add files via upload

---
+++ figura/avatars/First Birthday Skin Pack/Dragon Cape/script.lua
@@ -0,0 +1,4 @@
+-- Auto generated script file --
+
+--hide vanilla cape model
+vanilla_model.CAPE:setVisible(false)
---
+++ figura/avatars/First Birthday Skin Pack/Ender Dragon/script.lua
@@ -0,0 +1,7 @@
+-- Auto generated script file --
+
+--hide vanilla model
+vanilla_model.PLAYER:setVisible(false)
+
+--hide vanilla cape model
+vanilla_model.CAPE:setVisible(false)
